0a4d5e301ef2b9db09976849a54e361f9dca5cda
Jannis Pohlmann <jannis@edgeandnode.com>
2022-01-13 11:21:51 +0100

    Add database model and writing logic for POI cross-check reports

--- src/db/proofs_of_indexing.rs
+++ src/db/proofs_of_indexing.rs
@@ -6,7 +6,7 @@ use futures::{FutureExt, Stream, StreamExt, TryFutureExt};
 use futures_retry::{FutureRetry, RetryPolicy};
 use tracing::{info, warn};
 
-use crate::{indexer::Indexer, types};
+use crate::{db::models::POICrossCheckReport, indexer::Indexer, types};
 
 use super::{models::ProofOfIndexing, schema};
 
@@ -66,6 +66,81 @@ pub fn write<S, I>(
                 }
                 .map_err(|(error, attempts)| {
                     warn!(%error, %attempts, "Failed to write POIs to database");
+                })
+                .map(|_| ())
+            })
+            .await;
+    });
+}
+
+/// Write any POI cross-check reports that we receive to the database.
+pub fn write_reports<S, I>(
+    connection_pool: Arc<r2d2::Pool<r2d2::ConnectionManager<PgConnection>>>,
+    reports: S,
+) where
+    S: Stream<Item = types::POICrossCheckReport<I>> + Send + 'static,
+    I: Indexer + Send + Sync + 'static,
+{
+    tokio::spawn(async move {
+        reports
+            .ready_chunks(100)
+            .for_each(move |chunk: Vec<types::POICrossCheckReport<I>>| {
+                let connection_pool = connection_pool.clone();
+                let mut consecutive_errors = 0;
+
+                async move {
+                    FutureRetry::new(
+                        || async {
+                            let reports = chunk
+                                .clone()
+                                .into_iter()
+                                .map(|report| POICrossCheckReport {
+                                    indexer1: report
+                                        .poi1
+                                        .indexer
+                                        .id()
+                                        .trim_start_matches("0x")
+                                        .into(),
+                                    indexer2: report
+                                        .poi2
+                                        .indexer
+                                        .id()
+                                        .trim_start_matches("0x")
+                                        .into(),
+                                    deployment: report.poi1.deployment.to_string(),
+                                    block_hash: report.poi1.block.hash.to_string(),
+                                    block_number: report.poi1.block.number as i64,
+                                    proof_of_indexing1: report.poi1.proof_of_indexing.to_string(),
+                                    proof_of_indexing2: report.poi2.proof_of_indexing.to_string(),
+                                })
+                                .collect::<Vec<_>>();
+
+                            let number_of_reports = reports.len();
+
+                            let connection = connection_pool.get()?;
+                            diesel::insert_into(schema::poi_cross_check_reports::table)
+                                .values(reports)
+                                .on_conflict_do_nothing()
+                                .execute(&connection)?;
+
+                            info!(%number_of_reports, "Wrote POI cross-check reports to database");
+
+                            Ok(()) as Result<_, anyhow::Error>
+                        },
+                        |e| {
+                            if consecutive_errors >= 5 {
+                                consecutive_errors = 0;
+                                RetryPolicy::ForwardError(e)
+                            } else {
+                                consecutive_errors += 1;
+                                RetryPolicy::WaitRetry(Duration::from_secs(1))
+                            }
+                        },
+                    )
+                    .await
+                }
+                .map_err(|(error, attempts)| {
+                    warn!(%error, %attempts, "Failed to write POI cross-check reports to database");
                 })
                 .map(|_| ())
             })
--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,9 +1,23 @@
 table! {
-    proofs_of_indexing (indexer, deployment, block_number) {
+    proofs_of_indexing (indexer, deployment, block_hash) {
+        timestamp -> Timestamp,
         indexer -> Varchar,
         deployment -> Varchar,
         block_number -> Int8,
         block_hash -> Varchar,
         proof_of_indexing -> Varchar,
     }
+}
+
+table! {
+     poi_cross_check_reports (indexer1, indexer2, deployment, block_hash) {
+         timestamp -> Timestamp,
+         indexer1 -> Varchar,
+         indexer2 -> Varchar,
+         deployment-> Varchar,
+         block_number -> Int8,
+         block_hash -> Varchar,
+         proof_of_indexing1 -> Varchar,
+         proof_of_indexing2 -> Varchar,
+     }
 }
