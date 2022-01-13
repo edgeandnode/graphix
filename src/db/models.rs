fd9bd4f57e1644c33f4e9c025aabdef6744ac917
Jannis Pohlmann <jannis@edgeandnode.com>
2022-01-13 11:21:51 +0100

    Add database model and writing logic for POI cross-check reports

--- src/db/models.rs
+++ src/db/models.rs
@@ -10,3 +10,15 @@ pub struct ProofOfIndexing {
     pub block_hash: String,
     pub proof_of_indexing: String,
 }
+
+#[derive(Debug, Insertable, Queryable)]
+#[table_name = "poi_cross_check_reports"]
+pub struct POICrossCheckReport {
+    pub indexer1: String,
+    pub indexer2: String,
+    pub deployment: String,
+    pub block_number: i64,
+    pub block_hash: String,
+    pub proof_of_indexing1: String,
+    pub proof_of_indexing2: String,
+}
