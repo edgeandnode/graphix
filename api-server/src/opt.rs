d7e2473f575fa782b434385421be31552ac86278
Jannis Pohlmann <jannis@edgeandnode.com>
2022-01-21 13:46:35 +0100

    Add basic GraphQL resolver for querying POIs
    This will likely change as we add a frontend and better understand what
    types of queries it needs to make.

--- api-server/src/opt.rs
+++ api-server/src/opt.rs
@@ -4,6 +4,9 @@ use structopt::StructOpt;
 pub struct Options {
     #[structopt(long, default_value = "80")]
     pub port: u16,
+
+    #[structopt(long, default_value = "postgresql://localhost:5432/graph-ixi")]
+    pub database_url: String,
 }
 
 pub fn options_from_args() -> Options {
--- api-server/src/schema.rs
+++ api-server/src/schema.rs
@@ -1,17 +1,104 @@
-use anyhow;
-use async_graphql::{Context, EmptyMutation, EmptySubscription, Object, Schema};
+use std::sync::Arc;
+
+use async_graphql::{
+    Context, EmptyMutation, EmptySubscription, InputObject, Object, Schema, SimpleObject,
+};
+use diesel::{
+    r2d2::{ConnectionManager, Pool},
+    ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl,
+};
+use graph_ixi_common::db::{self, models};
 
 pub struct QueryRoot;
 
-#[Object]
-impl QueryRoot {
-    async fn value(&self, ctx: &Context<'_>) -> Result<i32, anyhow::Error> {
-        Ok(1)
+#[derive(InputObject)]
+struct ProofOfIndexingRequest {
+    deployments: Vec<String>,
+    block_range: Option<BlockRange>,
+    limit: Option<u16>,
+}
+
+#[derive(InputObject)]
+struct BlockRange {
+    start: u64,
+    end: u64,
+}
+
+#[derive(SimpleObject)]
+struct PartialBlock {
+    number: i64,
+    hash: Option<String>,
+}
+
+#[derive(SimpleObject)]
+struct ProofOfIndexing {
+    timestamp: String,
+    deployment: String,
+    indexer: String,
+    proof_of_indexing: String,
+    block: PartialBlock,
+}
+
+impl From<models::ProofOfIndexing> for ProofOfIndexing {
+    fn from(poi: models::ProofOfIndexing) -> Self {
+        Self {
+            timestamp: poi.timestamp.to_string(),
+            deployment: poi.deployment,
+            indexer: poi.indexer,
+            block: PartialBlock {
+                number: poi.block_number,
+                hash: poi.block_hash,
+            },
+            proof_of_indexing: poi.proof_of_indexing,
+        }
     }
 }
 
-pub type ApiSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+#[Object]
+impl QueryRoot {
+    async fn pois(
+        &self,
+        ctx: &Context<'_>,
+        request: ProofOfIndexingRequest,
+    ) -> Result<Vec<ProofOfIndexing>, async_graphql::Error> {
+        use db::schema::proofs_of_indexing::dsl::*;
 
-pub fn api_schema() -> ApiSchema {
-    Schema::new(QueryRoot, EmptyMutation, EmptySubscription)
+        let api_ctx = ctx.data::<APISchemaContext>()?;
+        let connection = api_ctx.db_connection_pool.get()?;
+
+        let query = proofs_of_indexing
+            .order_by(block_number.desc())
+            .order_by(timestamp.desc())
+            .filter(deployment.eq_any(&request.deployments))
+            .filter(
+                block_number.between(
+                    request
+                        .block_range
+                        .as_ref()
+                        .map_or(0, |range| range.start as i64),
+                    request
+                        .block_range
+                        .map_or(i64::max_value(), |range| range.end as i64),
+                ),
+            )
+            .limit(request.limit.unwrap_or(1000) as i64);
+
+        Ok(query
+            .load::<models::ProofOfIndexing>(&connection)?
+            .into_iter()
+            .map(ProofOfIndexing::from)
+            .collect())
+    }
+}
+
+pub type APISchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+
+pub struct APISchemaContext {
+    pub db_connection_pool: Arc<Pool<ConnectionManager<PgConnection>>>,
+}
+
+pub fn api_schema(ctx: APISchemaContext) -> APISchema {
+    Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(ctx)
+        .finish()
 }
