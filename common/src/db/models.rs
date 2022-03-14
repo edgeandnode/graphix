9ee7c5f6cea86dd6dbe698f97160f73b87aa4625
Jannis Pohlmann <jannis@edgeandnode.com>
2022-03-14 21:27:06 +0100

    Add diverging block data and POI report API

--- common/src/db/models.rs
+++ common/src/db/models.rs
@@ -1,5 +1,16 @@
+use std::io::Write;
+
 use chrono::NaiveDateTime;
-use diesel::{Insertable, Queryable};
+use diesel::{
+    pg::Pg,
+    serialize::Output,
+    sql_types::Jsonb,
+    types::{FromSql, ToSql},
+    Insertable, Queryable,
+};
+use serde::{Deserialize, Serialize};
+
+use crate::types;
 
 use super::schema::*;
 
@@ -14,6 +25,40 @@ pub struct ProofOfIndexing {
     pub proof_of_indexing: String,
 }
 
+#[derive(FromSqlRow, AsExpression, Serialize, Deserialize, Debug, Default)]
+#[sql_type = "Jsonb"]
+pub struct DivergingBlock {
+    pub block_number: i64,
+    pub block_hash: Option<String>,
+    pub proof_of_indexing1: String,
+    pub proof_of_indexing2: String,
+}
+
+impl From<types::DivergingBlock> for DivergingBlock {
+    fn from(block: types::DivergingBlock) -> Self {
+        Self {
+            block_number: block.block.number as i64,
+            block_hash: block.block.hash.map(|hash| hash.to_string()),
+            proof_of_indexing1: block.proof_of_indexing1.to_string(),
+            proof_of_indexing2: block.proof_of_indexing2.to_string(),
+        }
+    }
+}
+
+impl FromSql<Jsonb, Pg> for DivergingBlock {
+    fn from_sql(bytes: Option<&[u8]>) -> diesel::deserialize::Result<Self> {
+        let value = <serde_json::Value as FromSql<Jsonb, Pg>>::from_sql(bytes)?;
+        Ok(serde_json::from_value(value)?)
+    }
+}
+
+impl ToSql<Jsonb, Pg> for DivergingBlock {
+    fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> diesel::serialize::Result {
+        let value = serde_json::to_value(self)?;
+        <serde_json::Value as ToSql<Jsonb, Pg>>::to_sql(&value, out)
+    }
+}
+
 #[derive(Debug, Insertable, Queryable)]
 #[table_name = "poi_cross_check_reports"]
 pub struct POICrossCheckReport {
@@ -25,4 +70,5 @@ pub struct POICrossCheckReport {
     pub block_hash: Option<String>,
     pub proof_of_indexing1: String,
     pub proof_of_indexing2: String,
+    pub diverging_block: Option<DivergingBlock>,
 }
---
+++ migrations/2022-02-04-164229_add_diverging_block_field/down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE poi_cross_check_reports DELETE COLUMN diverging_block;
