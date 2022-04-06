0e37b3a5425df765c80cebb60b8791d58f6097b4
tilacog <tilacog@gmail.com>
2022-04-05 22:36:57 -0300

    cross-checker: Passing a unit value to a function

--- cross-checker/src/server/mod.rs
+++ cross-checker/src/server/mod.rs
@@ -3,5 +3,6 @@ use warp::Filter;
 pub async fn run() -> Result<(), anyhow::Error> {
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
 
-    Ok(warp::serve(hello).run(([127, 0, 0, 1], 3031)).await)
+    warp::serve(hello).run(([127, 0, 0, 1], 3031)).await;
+    Ok(())
 }
