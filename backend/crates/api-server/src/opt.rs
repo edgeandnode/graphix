f361b81bf4b40ab024ff99dbdcf6e3186d9ae541
Filippo Costa <filippo@edgeandnode.com>
2023-03-22 14:22:54 +0100

    backend: replace structopt with clap

--- backend/crates/api-server/src/opt.rs
+++ backend/crates/api-server/src/opt.rs
@@ -1,14 +1,10 @@
-use structopt::StructOpt;
+use clap::Parser;
 
-#[derive(StructOpt, Debug)]
-pub struct Options {
-    #[structopt(long, default_value = "80")]
+#[derive(Parser, Debug)]
+pub struct CliOptions {
+    #[clap(long, default_value = "80")]
     pub port: u16,
 
-    #[structopt(long, default_value = "postgresql://localhost:5432/graph-ixi")]
+    #[clap(long, default_value = "postgresql://localhost:5432/graph-ixi")]
     pub database_url: String,
-}
-
-pub fn options_from_args() -> Options {
-    Options::from_args()
 }
