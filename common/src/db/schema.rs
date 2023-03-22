9a8a7001af3ff2f5c9715947841ee11e7d19aaf2
Filippo Costa <filippo@edgeandnode.com>
2023-03-22 13:40:17 +0100

    common: make db internal schema code private

--- common/src/db/schema.rs
+++ common/src/db/schema.rs
@@ -10,15 +10,15 @@ table! {
 }
 
 table! {
-     poi_cross_check_reports (indexer1, indexer2, deployment, block_hash) {
-         timestamp -> Timestamp,
-         indexer1 -> Varchar,
-         indexer2 -> Varchar,
-         deployment-> Varchar,
-         block_number -> Int8,
-         block_hash -> Nullable<Varchar>,
-         proof_of_indexing1 -> Varchar,
-         proof_of_indexing2 -> Varchar,
-         diverging_block -> Nullable<Jsonb>,
-     }
+    poi_cross_check_reports (indexer1, indexer2, deployment, block_hash) {
+        timestamp -> Timestamp,
+        indexer1 -> Varchar,
+        indexer2 -> Varchar,
+        deployment-> Varchar,
+        block_number -> Int8,
+        block_hash -> Nullable<Varchar>,
+        proof_of_indexing1 -> Varchar,
+        proof_of_indexing2 -> Varchar,
+        diverging_block -> Nullable<Jsonb>,
+    }
 }
