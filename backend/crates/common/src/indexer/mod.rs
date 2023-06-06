b853db7cbe4202b7a667db049980d6d15b36e942
Leonardo Yvens <leoyvens@gmail.com>
2023-06-06 11:59:07 +0100

    Add test configuration for indexer interceptor

--- backend/crates/common/src/indexer/mod.rs
+++ backend/crates/common/src/indexer/mod.rs
@@ -1,5 +1,8 @@
 mod real_indexer;
 mod types;
 
+// A indexer interceptor, for test configs only.
+pub mod interceptor;
+
 pub use self::real_indexer::*;
 pub use self::types::*;
--- backend/crates/common/src/modes/testing.rs
+++ backend/crates/common/src/modes/testing.rs
@@ -1,9 +1,45 @@
+use std::sync::Arc;
+
 use tracing::*;
 
 use crate::config::TestingConfig;
-use crate::indexer::RealIndexer;
+use crate::indexer::interceptor::IndexerInterceptor;
+use crate::indexer::{Indexer, RealIndexer};
+use crate::prelude::EnvironmentConfig;
 
 #[instrument]
-pub fn testing_indexers(config: TestingConfig) -> Vec<RealIndexer> {
-    config.environments.iter().map(RealIndexer::new).collect()
+pub fn testing_indexers(config: TestingConfig) -> Vec<Arc<dyn Indexer>> {
+    let mut indexers: Vec<Arc<dyn Indexer>> = vec![];
+
+    // First, configure all the real indexers.
+    for config in &config.environments {
+        match config {
+            EnvironmentConfig::Indexer(config) => {
+                info!(indexer_id = %config.id, "Configuring indexer");
+                indexers.push(Arc::new(RealIndexer::new(config.clone())));
+            }
+            EnvironmentConfig::Interceptor(_) => {}
+        }
+    }
+
+    // Then, configure all the interceptors, referring to the real indexers by ID.
+    for config in config.environments {
+        match config {
+            EnvironmentConfig::Indexer(_config) => {}
+            EnvironmentConfig::Interceptor(config) => {
+                info!(interceptor_id = %config.id, "Configuring interceptor");
+                let target = indexers
+                    .iter()
+                    .find(|indexer| indexer.id() == config.target)
+                    .expect("interceptor target indexer not found");
+                indexers.push(Arc::new(IndexerInterceptor::new(
+                    config.id,
+                    target.clone(),
+                    config.poi_byte,
+                )));
+            }
+        }
+    }
+
+    indexers
 }
