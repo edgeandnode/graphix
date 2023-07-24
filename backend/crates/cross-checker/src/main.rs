b116c245f96d93575a3c74cecd9d17e1a9b6cbf4
Filippo Costa <filippo@neysofu.me>
2023-07-24 15:07:01 +0200

    Allow multiple network subgraphs and queries

--- backend/crates/cross-checker/src/main.rs
+++ backend/crates/cross-checker/src/main.rs
@@ -11,6 +11,7 @@ use graphix_common::queries::{query_indexing_statuses, query_proofs_of_indexing}
 use graphix_common::PrometheusExporter;
 use graphix_common::{config, store};
 use prometheus_exporter::prometheus;
+use std::collections::HashSet;
 use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Duration;
@@ -27,17 +28,20 @@ async fn main() -> anyhow::Result<()> {
     info!("Parse options");
     let cli_options = CliOptions::parse();
 
-    info!("Load configuration file");
+    info!("Loading configuration file");
     let config = Config::read(&cli_options.config)?;
 
+    info!("Initialize store and running migrations");
     let store = store::Store::new(&config.database_url).await?;
+    info!("Store initialization successful");
 
     let sleep_duration = Duration::from_secs(config.polling_period_in_seconds);
 
     // Prometheus metrics.
     let registry = prometheus::default_registry().clone();
-    let _exporter = PrometheusExporter::start(9184, registry.clone()).unwrap();
+    let _exporter = PrometheusExporter::start(config.prometheus_port, registry.clone()).unwrap();
 
+    info!("Initializing bisect request handler");
     let store_clone = store.clone();
     let (tx_indexers, rx_indexers) = watch::channel(vec![]);
     tokio::spawn(async move {
@@ -48,15 +52,21 @@ async fn main() -> anyhow::Result<()> {
 
     loop {
         info!("New main loop iteration");
-
         info!("Initialize inputs (indexers, indexing statuses etc.)");
-        let indexers = config::config_to_indexers(config.clone()).await?;
+
+        let mut indexers = config::config_to_indexers(config.clone()).await?;
+        // Different data sources, especially network subgraphs, result in
+        // duplicate indexers.
+        indexers = deduplicate_indexers(&indexers);
+
         tx_indexers.send(indexers.clone())?;
 
         let indexing_statuses = query_indexing_statuses(indexers).await;
 
         info!("Monitor proofs of indexing");
         let pois = query_proofs_of_indexing(indexing_statuses, config.block_choice_policy).await;
+
+        info!(pois = pois.len(), "Finished tracking PoIs");
 
         let write_err = store.write_pois(&pois, store::PoiLiveness::Live).err();
         if let Some(err) = write_err {
@@ -73,6 +83,18 @@ async fn main() -> anyhow::Result<()> {
 
 fn init_tracing() {
     tracing_subscriber::fmt::init();
+}
+
+fn deduplicate_indexers(indexers: &[Arc<dyn Indexer>]) -> Vec<Arc<dyn Indexer>> {
+    let mut seen = HashSet::new();
+    let mut deduplicated = vec![];
+    for indexer in indexers {
+        if !seen.contains(indexer.id()) {
+            deduplicated.push(indexer.clone());
+            seen.insert(indexer.id().to_string());
+        }
+    }
+    deduplicated
 }
 
 #[derive(Parser, Debug)]
