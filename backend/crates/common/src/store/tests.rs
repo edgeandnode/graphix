04546677aeba9313cf8d2b95a998377491c855cb
Leonardo Yvens <leoyvens@gmail.com>
2023-08-08 20:43:26 -0300

    tests: Refactor for more code reuse

--- backend/crates/common/src/store/tests.rs
+++ backend/crates/common/src/store/tests.rs
@@ -1,14 +1,14 @@
-use std::collections::BTreeSet;
-
-use diesel::Connection;
-
 use crate::block_choice::BlockChoicePolicy;
 use crate::graphql_api::types::SgDeploymentsQuery;
+use crate::prelude::ProofOfIndexing;
 use crate::prometheus_metrics::metrics;
-use crate::queries;
-use crate::store::{diesel_queries, PoiLiveness, Store};
-use crate::test_utils::fast_rng;
-use crate::test_utils::gen::gen_indexers;
+use crate::test_utils::{fast_rng, gen::gen_indexers};
+use crate::{
+    queries,
+    store::{diesel_queries, PoiLiveness, Store},
+};
+use diesel::Connection;
+use std::collections::BTreeSet;
 
 fn test_db_url() -> String {
     std::env::var("GRAPHIX_TEST_DB_URL").expect("GRAPHIX_TEST_DB_URL must be set to run tests")
@@ -68,45 +68,49 @@ async fn poi_db_roundtrip() {
 
     let store = Store::new(&test_db_url()).await.unwrap();
     let mut conn = store.conn().unwrap();
-    conn.test_transaction::<_, (), _>(|conn| {
-        diesel_queries::write_pois(conn, &pois.clone(), PoiLiveness::NotLive).unwrap();
-        let all_deployments: Vec<String> =
-            pois.iter().map(|poi| poi.deployment.0.clone()).collect();
-        let read_pois =
-            diesel_queries::pois(conn, None, Some(&all_deployments), None, None, false).unwrap();
+    conn.test_transaction(|conn| test_pois(conn, &pois, PoiLiveness::NotLive, false));
+    conn.test_transaction(|conn| test_pois(conn, &pois, PoiLiveness::Live, true));
+}
 
-        // The triple is (deployment, indexer_id, poi)
-        let poi_triples: BTreeSet<(String, String, Vec<u8>)> = pois
-            .clone()
-            .into_iter()
-            .map(|poi| {
-                (
-                    poi.deployment.0,
-                    poi.indexer.id().to_owned(),
-                    poi.proof_of_indexing.0.to_vec(),
-                )
-            })
-            .collect();
-        let read_poi_triples: BTreeSet<(String, String, Vec<u8>)> = read_pois
-            .into_iter()
-            .map(|poi| (poi.sg_deployment.cid, poi.indexer.name.unwrap(), poi.poi))
-            .collect();
-        assert!(poi_triples == read_poi_triples);
+fn test_pois(
+    conn: &mut diesel::PgConnection,
+    pois: &[ProofOfIndexing],
+    liveness: PoiLiveness,
+    live_poi_test: bool,
+) -> Result<(), anyhow::Error> {
+    diesel_queries::write_pois(conn, pois, liveness).unwrap();
+    let all_deployments: Vec<String> = pois.iter().map(|poi| poi.deployment.0.clone()).collect();
 
-        let live_pois =
-            diesel_queries::pois(conn, None, Some(&all_deployments), None, None, true).unwrap();
-        assert!(live_pois.is_empty());
+    // Common logic to create poi_triples
+    let poi_triples: BTreeSet<(String, String, Vec<u8>)> = pois
+        .clone()
+        .into_iter()
+        .map(|poi| {
+            (
+                poi.deployment.0.clone(),
+                poi.indexer.id().to_owned(),
+                poi.proof_of_indexing.0.to_vec(),
+            )
+        })
+        .collect();
+    let read_pois = diesel_queries::pois(
+        conn,
+        None,
+        Some(&all_deployments),
+        None,
+        None,
+        live_poi_test,
+    )
+    .unwrap();
+    let read_poi_triples: BTreeSet<(String, String, Vec<u8>)> = read_pois
+        .into_iter()
+        .map(|poi| (poi.sg_deployment.cid, poi.indexer.name.unwrap(), poi.poi))
+        .collect();
 
-        Ok(())
-    });
+    assert!(poi_triples == read_poi_triples);
 
-    // Same test as above, but this time the pois are live.
-    conn.test_transaction::<_, (), _>(|conn| {
-        diesel_queries::write_pois(conn, &pois.clone(), PoiLiveness::Live).unwrap();
-        let all_deployments: Vec<String> =
-            pois.iter().map(|poi| poi.deployment.0.clone()).collect();
-
-        // Assert that all pois are live pois
+    if live_poi_test {
+        // Specific test for live pois
         assert_eq!(
             diesel_queries::pois(conn, None, Some(&all_deployments), None, None, true)
                 .unwrap()
@@ -119,27 +123,12 @@ async fn poi_db_roundtrip() {
                 .map(|poi| poi.id)
                 .collect::<Vec<_>>()
         );
-
-        let read_pois =
+    } else {
+        // Specific test for not live pois
+        let live_pois =
             diesel_queries::pois(conn, None, Some(&all_deployments), None, None, true).unwrap();
+        assert!(live_pois.is_empty());
+    }
 
-        // The triple is (deployment, indexer_id, poi)
-        let poi_triples: BTreeSet<(String, String, Vec<u8>)> = pois
-            .clone()
-            .into_iter()
-            .map(|poi| {
-                (
-                    poi.deployment.0,
-                    poi.indexer.id().to_owned(),
-                    poi.proof_of_indexing.0.to_vec(),
-                )
-            })
-            .collect();
-        let read_poi_triples: BTreeSet<(String, String, Vec<u8>)> = read_pois
-            .into_iter()
-            .map(|poi| (poi.sg_deployment.cid, poi.indexer.name.unwrap(), poi.poi))
-            .collect();
-        assert!(poi_triples == read_poi_triples);
-        Ok(())
-    });
+    Ok(())
 }
