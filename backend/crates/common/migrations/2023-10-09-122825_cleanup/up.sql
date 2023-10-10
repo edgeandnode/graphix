048b8753cf73ddea773322e0019bf9377f39e7b7
Leonardo Yvens <leoyvens@gmail.com>
2023-10-10 16:50:54 +0100

    fix migrations and tests

--- backend/crates/common/migrations/2023-10-09-122825_cleanup/up.sql
+++ backend/crates/common/migrations/2023-10-09-122825_cleanup/up.sql
@@ -1,11 +1,10 @@
 -- Your SQL goes here
 
-DELETE * FROM divergence_investigation_reports;
-DELETE * FROM pending_divergence_investigation_requests;
-DELETE * FROM live_pois;
-DELETE * FROM pois;
-DELETE * FROM blocks;
-DELETE * FROM sg_names;
-DELETE * FROM sg_deployments;
-DELETE * FROM networks;
-DELETE * FROM indexers;
+DELETE FROM divergence_investigation_reports;
+DELETE FROM pending_divergence_investigation_requests;
+DELETE FROM live_pois;
+DELETE FROM pois;
+DELETE FROM blocks;
+DELETE FROM sg_names;
+DELETE FROM sg_deployments;
+DELETE FROM indexers;
