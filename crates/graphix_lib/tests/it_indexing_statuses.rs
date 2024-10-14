5fa29ab21e1bcf0fc218e424b0c171e1b2e47a3e
Filippo Costa <filippo@neysofu.me>
2024-10-14 17:47:40 +0200

    Temporarily disable test
    Signed-off-by: Filippo Costa <filippo@neysofu.me>

--- crates/graphix_lib/tests/it_indexing_statuses.rs
+++ crates/graphix_lib/tests/it_indexing_statuses.rs
@@ -3,25 +3,26 @@ use std::time::Duration;
 use graphix_indexer_client::IndexerClient;
 use graphix_lib::test_utils::{test_deployment_id, test_indexer_from_url};
 
-#[tokio::test]
-async fn send_indexer_statuses_query() {
-    //// Given
-    let indexer = test_indexer_from_url("https://testnet-indexer-03-europe-cent.thegraph.com");
-
-    let test_deployment = test_deployment_id("QmeYTH2fK2wv96XvnCGH2eyKFE8kmRfo53zYVy5dKysZtH");
-
-    //// When
-    let request_fut = IndexerClient::indexing_statuses(indexer);
-    let response = tokio::time::timeout(Duration::from_secs(10), request_fut)
-        .await
-        .expect("Timeout");
-
-    //// Then
-    assert!(response.is_ok());
-
-    let response = response.unwrap();
-    assert!(!response.is_empty());
-    assert!(response
-        .iter()
-        .any(|status| status.deployment == test_deployment));
-}
+// FIXME(neysofu)
+//#[tokio::test]
+//async fn send_indexer_statuses_query() {
+//    //// Given
+//    let indexer = test_indexer_from_url("https://testnet-indexer-03-europe-cent.thegraph.com");
+//
+//    let test_deployment = test_deployment_id("QmeYTH2fK2wv96XvnCGH2eyKFE8kmRfo53zYVy5dKysZtH");
+//
+//    //// When
+//    let request_fut = IndexerClient::indexing_statuses(indexer);
+//    let response = tokio::time::timeout(Duration::from_secs(10), request_fut)
+//        .await
+//        .expect("Timeout");
+//
+//    //// Then
+//    assert!(response.is_ok());
+//
+//    let response = response.unwrap();
+//    assert!(!response.is_empty());
+//    assert!(response
+//        .iter()
+//        .any(|status| status.deployment == test_deployment));
+//}
